2e87b35e315ef72b909e84aa8be410113bfbba61
Wiredos0 <sh70804050@gmail.com>
2024-10-16 23:48:36 +0600

    Update flux.js

--- scripts/cmds/flux.js
+++ scripts/cmds/flux.js
@@ -2,7 +2,7 @@ module.exports = {
   config: {
     name: "flux",
     author: "NZ R",
-    countDown: 10,
+    countDown: 60,
     category: "ai-generated",
     guide: {
       en: "{P}flux <prompt> --<ratio>\n\nOptional ratios:\n\n--1 for regular (1:1)\n--2 for 4:14\n--3 for 16:9\n\nExample: -flux 'your prompt' --1\n\n[FLUX.2 Model Image Generations]"
@@ -14,7 +14,7 @@ module.exports = {
     }
 
     let pr = a.join(" ");
-    if (!pr) return r("⛔ | Please provide a query for image generation.");
+    if (!pr) return r("Please provide a query for image generation.");
 
     let ratio = "1:1"; 
     let ratioName = "Regular (1:1)"; 
@@ -36,7 +36,7 @@ module.exports = {
 
     const requestStartTime = Date.now(); 
 
-    const waitingMessage = await r(" ⏰ | Generating image... Please wait...");
+    const waitingMessage = await r(" ⏳ | Generating image, Please wait...");
     const waitingMessageID = waitingMessage.messageID; 
 
     try {
