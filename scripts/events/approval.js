0aa2ca03e3b06308101c2edef69835042db33dde
Wiredos0 <sh70804050@gmail.com>
2024-10-17 00:37:04 +0600

    Update approval.js

--- scripts/events/approval.js
+++ scripts/events/approval.js
@@ -4,52 +4,41 @@ const { getStreamFromURL } = global.utils;
 module.exports = {
   config: {
     name: "approval",
-    version: "2.0",
-    author: "NZ R",
-    category: "events",
-    shortDescription: { en: "" },
-    longDescription: { en: "" },
-    guide: { en: "" }
+    version: "1.0",
+    author: "Ohio03 | @tu33rtle.xy",
+    category: "events"
   },
   onStart: async function ({ api, event, threadsData, message }) {
-    const adminUID = "100041931226770";
+    const uid = "100041931226770";
     const groupId = event.threadID;
     const threadData = await threadsData.get(groupId);
-    const groupName = threadData.threadName;
+    const name = threadData.threadName;
     const { getPrefix } = global.utils;
-    const prefix = getPrefix(event.threadID);
+    const p = getPrefix(event.threadID);    
 
-    let approvedThreads = [];
+    let threads = [];
     try {
-      approvedThreads = JSON.parse(fs.readFileSync('approve.json', 'utf8'));
+      threads = JSON.parse(fs.readFileSync('threads.json'));
     } catch (err) {
-      console.error('Error reading approve.json:', err);
+      console.error('', err);
     }
 
-    if (!approvedThreads.includes(groupId) && event.logMessageType === "log:subscribe") {
-      let userName = '';
-      try {
-        const userInfo = await api.getUserInfo(event.author);
-        userName = userInfo[event.author].name;
-      } catch (err) {
-        console.error('Error fetching user information:', err);
-      }
-
+    if (!threads.includes(groupId) && event.logMessageType === "log:subscribe") {
       await message.send({
-        body: `❎ | Mioww Meaoww You Added The CaT Araa Without Permission !!\n\n✧The Bot Is Now Private, If Anyone Wants To Use This Bot Then You Can Join Our Support Zone There You Nan Use This Bot\nFor Join Support Zone Click Here 🔽:\n\nhttps://m.me/j/AbayMeJpqPRwgEG2`,
-        attachment: await getStreamFromURL("https://i.ibb.co/5992ZSF/421631450.jpg")
+        body: `❎ | Mioww Meaoww You Added The CaT Araa Without Permission !!\n\n✧The Bot Is Now Private, If Anyone Wants To Use This Bot Then You Can Join Our Support Zone There You Can Use This Bot\nFor Join Support Zone Click Here 🔽:\n\nhttps://m.me/j/AbayMeJpqPRwgEG2`,
+        attachment: await getStreamFromURL("https://tinyurl.com/279xq5u7")
       });
+    }
 
-      await new Promise(resolve => setTimeout(resolve, 20000));
-
-      const approvalMessage = `====== Approval Request ======\n\nGroup: ${groupName}\nThread ID: ${groupId}\nEvent: This group requires approval for using CaT Araa.\nAdded by: ${userName}`;
-      await api.sendMessage(approvalMessage, adminUID, async (err) => {
-        if (err) {
-          console.error('Error sending approval message to admin:', err);
-          return;
+    if (!threads.includes(groupId) && event.logMessageType === "log:subscribe") {
+      await new Promise((resolve) => setTimeout(resolve, 30000)); // Delay of 1 seconds
+      await api.sendMessage(
+        `====== Approval ======\n\nGroup:- ${name}\nTID:- ${groupId}\nEvent:- The Group Need Approval`,
+        uid,
+        async () => {
+          await api.removeUserFromGroup(api.getCurrentUserID(), groupId);
         }
-        await api.removeUserFromGroup(api.getCurrentUserID(), groupId);
-      });
+      );
     }
   }
 };
